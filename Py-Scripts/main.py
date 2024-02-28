f10f6a4669a0f276cda63cf55d06a2a588d6fbc7
unknown <diaz.dungo@gmail.com>
2024-02-28 16:11:38 +0800

    Add large files tracked by Git LFS

---
+++ Py-Scripts/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c83e61e7e213e31f4f33a00a3ea720ea6a89e80ade39ae3fd78ee91b1d571ca3
+size 6803
---
+++ mail.js
@@ -0,0 +1,46 @@
+class MailSender{
+    nodemailer;
+    gmail;
+    mailfrom;
+    tomail;
+    sub;
+    message;
+    
+    constructor(to, sub, msg){
+        this.mailfrom = 'facetraceattendancesystem@gmail.com';
+        this.tomail = to;
+        this.sub = sub;
+        this.message = msg;
+        this.nodemailer = require('nodemailer');
+        this.gmail = this.nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'facetraceattendancesystem@gmail.com',
+              pass: 'group8bsit4-1n'
+            }
+          });
+        console.log('Mail obj created')
+    }
+
+    send(){
+      console.log("sending mail")
+      var mailOptions = {
+        from: this.mailfrom,
+        to: this.tomail,
+        subject: this.sub,
+        html: this.message,
+      };
+      
+      this.gmail.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+  };
+
+}
+
+module.exports = MailSender;
+
