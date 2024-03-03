4b236a34aa8bf079f07be43180d39326fbcdff37
junedungo <77611197+junedungo@users.noreply.github.com>
2024-03-03 15:41:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,14 +20,7 @@ var classrouter = require('./routes/classroom')
 
 var app = express();
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/attendance_portal';
-
-console.log('MongoDB URI:', mongoURI); // Add this line for debugging
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err)); // Use console.error for better visibility of errors
-
+mongoose.connect('mongodb://localhost:27017/attendance_portal',{useNewUrlParser: true, useUnifiedTopology: true})
 
 require('./config/passport');
 
